772c1e810d3fd1bcc69fb293355f5e02d60bc41a
Sulyma Mykhailo <msulima@magecore.com>
2015-03-16 16:05:57 +0200

    OOT-514 Added blank CRUD controllers with ACL and their view for entity 'Issue'

--- Bundle/BugTrackingSystemBundle/Controller/IssueController.php
+++ Bundle/BugTrackingSystemBundle/Controller/IssueController.php
@@ -7,6 +7,10 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
+use Oro\Bundle\BugTrackingSystemBundle\Entity\Issue;
+use Oro\Bundle\SecurityBundle\Annotation\Acl;
+use Oro\Bundle\SecurityBundle\Annotation\AclAncestor;
+
 class IssueController extends Controller
 {
     /**
@@ -14,9 +18,75 @@ class IssueController extends Controller
      *      "/",
      *      name="oro_bug_tracking_system_issue_index"
      * )
+     * @Acl(
+     *      id="oro_bug_tracking_system_issue_view",
+     *      type="entity",
+     *      class="OroBugTrackingSystemBundle:Issue",
+     *      permission="VIEW"
+     * )
      * @Template
+     *
+     * @return array
      */
     public function indexAction()
+    {
+        return [
+            'entity_class' => $this->container->getParameter('oro_bug_tracking_system.issue.entity.class')
+        ];
+    }
+
+    /**
+     * @Route(
+     *      "/create",
+     *      name="oro_bug_tracking_system_issue_create"
+     * )
+     * @Acl(
+     *      id="oro_bug_tracking_system_issue_create",
+     *      type="entity",
+     *      class="OroBugTrackingSystemBundle:Issue",
+     *      permission="CREATE"
+     * )
+     * @Template
+     *
+     * @return array
+     */
+    public function createAction()
+    {
+        return [];
+    }
+
+    /**
+     * @Route(
+     *      "/view/{id}",
+     *      name="oro_bug_tracking_system_issue_view",
+     *      requirements={"id"="\d+"}
+     * )
+     * @AclAncestor("oro_bug_tracking_system_issue_view")
+     * @Template
+     *
+     * @param Issue $issue
+     * @return array
+     */
+    public function viewAction(Issue $issue)
+    {
+        return ['entity' => $issue];
+    }
+
+    /**
+     * @Route(
+     *      "/update/{id}",
+     *      name="oro_bug_tracking_system_issue_update",
+     *      requirements={"id"="\d+"}
+     * )
+     * @Acl(
+     *      id="oro_bug_tracking_system_issue_update",
+     *      type="entity",
+     *      class="OroBugTrackingSystemBundle:Issue",
+     *      permission="EDIT"
+     * )
+     * @Template
+     */
+    public function updateAction(Issue $issue)
     {
         return [];
     }
